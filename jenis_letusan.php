2cbc041315b834de35c930779cbf555e8a2a130c
ahmadiqballl <comebandage@gmail.com>
2025-11-24 17:38:10 +0700

    Update jenis_letusan.php

--- jenis_letusan.php
+++ jenis_letusan.php
@@ -6,7 +6,7 @@
     <title>Jenis Letusan - Nada Vulkanik</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="css/jenisletusan.css">
+    <link rel="stylesheet" href="css/jenis_letusan.css">
 </head>
 <body>
     <!-- Navbar -->
@@ -200,3 +200,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
